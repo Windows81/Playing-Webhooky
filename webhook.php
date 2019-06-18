54a68245abd0c1975e6089fc8698f7b73b2acba4
1630228 <nastyahmede@gmail.com>
2019-06-18 02:12:18 +0000

    Fixed SQL data entry.

--- webhook.php
+++ webhook.php
@@ -18,7 +18,7 @@ $is=$ua==='Roblox/WinInet'?1:0;
 
 if(isset($stuff['embeds']))$contentSQL=json_encode($stuff['embeds']);
 else$contentSQL=$stuff['content'];
-$contentSQL=preg_replace("/([^\\\\]|^)[']/","$1\'",$contentSQL);
+//$contentSQL=preg_replace("/([^\\\\]|^)[']/","$1\'",$contentSQL);
 
 $main=convert(
 	$stuff['url']??
@@ -428,7 +428,7 @@ if($cat==24){
 $db=!in_array($cat,array(4,13));
 if($cat==3&&strpos($contentSQL,'Sanity Check')!==false)$db=false;
 if($cat==33&&strpos($contentSQL,'Player Joined')===false)$db=false;
-if($db)($sqli=new mysqli('localhost','id152849_windows10','YourSQL','id152849_windows10'))->query("INSERT INTO `Informations` (`Content`,`Webhook`,`PlaceID`,`Category`,`IsServer`) VALUES ('$contentSQL','$main',$id,$cat,$is);");
+if($db)($sqli=new mysqli('localhost','id152849_windows10','YourSQL','id152849_windows10'))->query("INSERT INTO `Informations` (`Content`,`Webhook`,`PlaceID`,`Category`,`IsServer`) VALUES ('".$sqli->$mysqli->real_escape_string($contentSQL)."','$main',$id,$cat,$is);");
 
 if($status)http_response_code($status);
 //echo$sS;
